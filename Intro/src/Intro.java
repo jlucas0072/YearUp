4b4e8b53060638aaad3ed79bf1652b3dccf34ffc
jlucas0072 <jlucas@atl.yearup.org>
2015-04-24 09:57:44 -0400

    First java project.

---
+++ Intro/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World! I'm Jordan Lucas. Welcome to the java class.");
+	}
+//When I remove a bracket I get an error sign next to a the line number 
+	//and an error message in the dialog box.
+	// If I remove the class, the dialog box tells me it can not load of find the man class Intro
+	//If I delete the word void, a dailog box appears and tells me there is a NJI error
+}
