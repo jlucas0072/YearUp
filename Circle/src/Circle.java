0d36949b96b449f4a8a8af5437a61f27d0d62fb0
jlucas0072 <jlucas@atl.yearup.org>
2015-05-07 23:08:55 -0400

    Java Project 3 Circle class

---
+++ Circle/src/Circle.java
@@ -0,0 +1,29 @@
+
+public class Circle {
+
+	String m_color = null;
+	double m_radius = -1;
+	
+	Circle(double radius)
+	{
+		m_radius = radius;
+		m_color = "grey";
+	}
+	
+	Circle(double radius, String color)
+	{
+		m_radius = radius;
+		m_color = color;
+	}
+	
+	public double CalculateArea() {
+		double area = 3.14 * m_radius * m_radius;
+		return area;
+	}
+
+	public void PrintColor() {
+		System.out.println("my color is: " + m_color);
+	}
+	
+	
+}
