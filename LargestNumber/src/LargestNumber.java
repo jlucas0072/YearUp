0b0363f5289f6c730a6ef92d2a9cf11266ce991a
jlucas0072 <jlucas@atl.yearup.org>
2015-05-29 22:33:51 -0400

    Sorts an array to find the largest number using for and while loops.

---
+++ LargestNumber/src/LargestNumber.java
@@ -0,0 +1,93 @@
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		
+		int[] anArray = new int[5];
+		anArray[0] = 85;
+		anArray[1] = 29;
+		anArray[2] = 34;
+		anArray[3] = 110;
+		anArray[4] = 43;
+		
+		int[] emptyArray = {};
+		int[] nullArray = null;
+		
+
+		LargestNumber newLN = new LargestNumber();
+		System.out.println("This finds the largest number using a for loop.");
+		newLN.LargestNumberUsingForLoop(anArray);
+		System.out.println("This finds the largest number using a while loop.");
+		newLN.LargestNumberUsingWhileLoop(anArray);
+		System.out.println("Empty array test: ");
+		newLN.LargestNumberUsingForLoop(emptyArray);
+		newLN.LargestNumberUsingForLoop(emptyArray);
+		System.out.println("Null array test: ");
+		newLN.LargestNumberUsingForLoop(nullArray);
+		newLN.LargestNumberUsingForLoop(nullArray);
+
+	}
+	
+	public void LargestNumberUsingForLoop (int[] anArray)
+	{
+		if (anArray != null)
+		{
+			int largest = -1;
+			
+			for(int i = 0; i < anArray.length; i++)
+			{
+				int current = anArray[i];
+
+				if (current > largest)
+				{
+					largest = current;
+				}
+
+			}
+			
+			System.out.println(largest);
+		}
+		
+		else
+		{
+			System.out.println("You gave me a null array.");
+		}
+	}
+	
+	public void LargestNumberUsingWhileLoop (int[] anArray)
+	{
+		
+			if (anArray != null)
+			{			
+				if (anArray.length > 0)
+				{
+					int j = 0;
+					int largest = anArray[0];
+					
+					
+					
+					//while (current < anArray[j])
+					while (anArray[j] < anArray.length)
+					{
+						int current = anArray[j];
+						if (current > largest)
+						{
+							largest = current;
+						}
+					}
+					j++;
+				System.out.println(largest);
+				}
+				else
+				{
+					System.out.println("This array does not have any numbers.");
+				}
+			}	
+			else
+			{
+				System.out.println("You gave me a null array.");
+			}
+
+	}
+
+}
